63f162a6545c2a891162903e3e7636a71e618da3
WXY <yisusheng666@gmail.com>
2022-01-11 18:00:35 +0800

    Create FixedCapacityStackOfStrings.cs

---
+++ src/algs/algs/FixedCapacityStackOfStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace algs
+{
+    class FixedCapacityStackOfStrings<Item>
+    {
+        private Item[] a;
+        private int N;
+        public FixedCapacityStackOfStrings(int cap)
+        { a = new Item[cap]; }
+        public bool isEmpty() { return N == 0; }
+        public int size() { return N; }
+        public void push(Item item)
+        { a[N++] = item; }
+        public Item pop()
+        { return a[N--]; }
+    }
+}
