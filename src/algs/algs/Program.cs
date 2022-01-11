3840a4b376ced5f8cf376b421661d852d509f977
WXY <yisusheng666@gmail.com>
2022-01-11 17:59:40 +0800

    添加项目

---
+++ src/algs/algs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
